73987ab8ed625b93a4a4dfcf547957a8a684ae83
Rohit Narurkar <rohit.narurkar@protonmail.com>
2023-03-14 14:35:53 +0100

    test/benchmarking for blake2f circuit

--- blake2f-circuit/src/lib.rs
+++ blake2f-circuit/src/lib.rs
@@ -80,7 +80,129 @@ impl<F: FieldExt> Blake2fChip<F> {
 }
 
 #[cfg(any(feature = "test", test))]
-pub mod dev {}
+pub mod dev {
+    use super::*;
+
+    use ethers_core::{types::H512, utils::hex::FromHex};
+    use halo2_proofs::{arithmetic::FieldExt, circuit::SimpleFloorPlanner, plonk::Circuit};
+    use std::{marker::PhantomData, str::FromStr};
+
+    lazy_static::lazy_static! {
+        // https://eips.ethereum.org/EIPS/eip-152#example-usage-in-solidity
+        pub static ref INPUTS_OUTPUTS: (Vec<Blake2fTestCircuitArgs>, Vec<H512>) = {
+            let (h1, h2) = (
+                <[u8; 32]>::from_hex("48c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5").expect(""),
+                <[u8; 32]>::from_hex("d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b").expect(""),
+            );
+            let (m1, m2, m3, m4) = (
+                <[u8; 32]>::from_hex("6162630000000000000000000000000000000000000000000000000000000000").expect(""),
+                <[u8; 32]>::from_hex("0000000000000000000000000000000000000000000000000000000000000000").expect(""),
+                <[u8; 32]>::from_hex("0000000000000000000000000000000000000000000000000000000000000000").expect(""),
+                <[u8; 32]>::from_hex("0000000000000000000000000000000000000000000000000000000000000000").expect(""),
+            );
+            (
+                vec![
+                    Blake2fTestCircuitArgs {
+                        rounds: 12,
+                        h: [
+                            u64::from_le_bytes(h1[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(h1[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(h1[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(h1[0x18..0x20].try_into().expect("")),
+                            u64::from_le_bytes(h2[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(h2[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(h2[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(h2[0x18..0x20].try_into().expect("")),
+                        ],
+                        m: [
+                            u64::from_le_bytes(m1[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(m1[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(m1[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(m1[0x18..0x20].try_into().expect("")),
+                            u64::from_le_bytes(m2[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(m2[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(m2[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(m2[0x18..0x20].try_into().expect("")),
+                            u64::from_le_bytes(m3[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(m3[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(m3[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(m3[0x18..0x20].try_into().expect("")),
+                            u64::from_le_bytes(m4[0x00..0x08].try_into().expect("")),
+                            u64::from_le_bytes(m4[0x08..0x10].try_into().expect("")),
+                            u64::from_le_bytes(m4[0x10..0x18].try_into().expect("")),
+                            u64::from_le_bytes(m4[0x18..0x20].try_into().expect("")),
+                        ],
+                        t: [3, 0],
+                        f: true,
+                    }
+                ],
+                vec![
+                    H512::from_str("ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923")
+                    .expect("BLAKE2F compression function output is 64-bytes")
+                ],
+            )
+        };
+    }
+
+    #[derive(Clone, Default)]
+    pub struct Blake2fTestCircuitArgs {
+        pub rounds: u32,
+        pub h: [u64; 8],
+        pub m: [u64; 16],
+        pub t: [u64; 2],
+        pub f: bool,
+    }
+
+    #[derive(Default)]
+    pub struct Blake2fTestCircuit<F> {
+        pub inputs: Vec<Blake2fTestCircuitArgs>,
+        pub outputs: Vec<H512>,
+        pub _marker: PhantomData<F>,
+    }
+
+    impl<F: FieldExt> Circuit<F> for Blake2fTestCircuit<F> {
+        type Config = Blake2fConfig<F>;
+        type FloorPlanner = SimpleFloorPlanner;
+
+        fn without_witnesses(&self) -> Self {
+            Self::default()
+        }
+
+        fn configure(meta: &mut halo2_proofs::plonk::ConstraintSystem<F>) -> Self::Config {
+            let blake2f_table = Blake2fTable::construct(meta);
+            Blake2fConfig::configure(meta, blake2f_table)
+        }
+
+        fn synthesize(
+            &self,
+            config: Self::Config,
+            mut layouter: impl Layouter<F>,
+        ) -> Result<(), Error> {
+            let chip = Blake2fChip::construct(config);
+            chip.load(&mut layouter)
+        }
+    }
+}
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use halo2_proofs::{dev::MockProver, halo2curves::bn256::Fr};
+    use std::marker::PhantomData;
+
+    use crate::dev::{Blake2fTestCircuit, INPUTS_OUTPUTS};
+
+    #[test]
+    fn test_blake2f_circuit() {
+        let (inputs, outputs) = INPUTS_OUTPUTS.clone();
+
+        let circuit: Blake2fTestCircuit<Fr> = Blake2fTestCircuit {
+            inputs,
+            outputs,
+            _marker: PhantomData,
+        };
+
+        let k = 8;
+        let prover = MockProver::run(k, &circuit, vec![]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
+    }
+}
